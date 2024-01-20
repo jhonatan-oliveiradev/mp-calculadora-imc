b7d671c2946cf62f4f5e304dfe30ef3250251119
jhonatan-oliveiradev <jhonatan.oliveira931002@gmail.com>
2024-01-20 14:06:28 -0300

    :construction: feat: Basic components and initial styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
-import "./App.css";
+import { Form } from "./components/Form";
+import { ReferenceTable } from "./components/ReferenceTable";
+import { Result } from "./components/Result";
 
 function App() {
 	return (
-		<main>
-			<h1>Calculadora de IMC</h1>
+		<main className="bg-white max-w-4xl mx-auto py-24 px-48">
+			<Form />
+			<Result />
+			<ReferenceTable />
 		</main>
 	);
 }
---
+++ src/components/Result.tsx
@@ -0,0 +1,9 @@
+export const Result = () => {
+	return (
+		<section id="result" className="py-10 px-40 h-48">
+			<p className="text-center text-neutral-400">
+				Saiba agora se está no seu peso ideal!
+			</p>
+		</section>
+	);
+};
